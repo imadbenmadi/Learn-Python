c4625dd1640693e38f3dc527cc5624ebac4e33a6
imadbenmadi <imadbenmadi891@gmail.com>
2024-12-04 23:50:08 +0100

    Update virtual environment paths and add .gitignore for venv

--- Assignment 2/venv/Scripts/activate.bat
+++ Assignment 2/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\imadb\OneDrive\Bureau\Python\Assignment 2\venv
+set VIRTUAL_ENV=C:\Users\imadb\OneDrive\Bureau\Learn-Python\Assignment 2\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
